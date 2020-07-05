2eb300fa09fce0c4d29c969982526e5bbf3eed80
dmitriy <dima.lunev14@gmail.com>
2020-07-05 19:09:54 +0300

    исправил баг с invalid path в Linux

--- Lesson_0_6 - os, os.path/path_tree.py
+++ Lesson_0_6 - os, os.path/path_tree.py
@@ -1,16 +1,19 @@
-import os, os.path
+import os, os.path, platform
 from random import randint, random
 
 
-def mkranddir(place:str, deep:int, num:int) -> None:
+console_print = None
+
+
+def mkranddir(place:str, deep:int, num:int) -> console_print:
     """Make path to random maked dir with folders and text files, return None
-    
+
     Keyword arguments:
     place -- path to place, where dir will be made (default C:\\Users\\dimal\\Desctop\\test)
     deep -- number of levels, which program will run to make dirs (default 1)
     num -- number of folders+files on every deep level (default 5)
     """
-    
+
     # validation of args
     if not place or type(place) is not str or not os.path.exists(place):
         print('===================================================================',\
@@ -48,21 +51,24 @@ def mkranddir(place:str, deep:int, num:int) -> None:
         mkranddir(place+'\\'+ls, deep+1, num)
 
 
-def tree(path:str, avoid_sys_dirs:bool, level=1) -> None:
+def tree(path:str, avoid_sys_dirs:bool, level=1) -> console_print:
     """walk through path and draw tree, return None
-    
+
     Keyword arguments:
     path -- place, which will be drew (default C:\\python)
     avoid_sys_dirs -- will ignore folders, starting with __ or . or $ (default True)
     level -- show the depth of recursion in function and is used for print (default 1)
     """
-    
+
     # validation of args
     if not path or type(path) is not str or not os.path.exists(path):
-        print('==================================================================',\
-              '\n= Something went wrong, so I override path to default: C:\\python =',\
-              '\n==================================================================\n\n', sep ='')
-        path = 'C:\\python'
+        print('Something went wrong, so I override path to default')
+        if platform.system() == 'Linux':
+            path = '/home/' + os.listdir('/home')[1]
+        elif platform.system() == 'Windows':
+            path = 'D:\\'
+        else:
+            raise Exception("Not supported OS...")
     if not avoid_sys_dirs or type(avoid_sys_dirs) is not bool:
         avoid_sys_dirs = True
     if level < 1 or type(level) is not int:
@@ -71,30 +77,32 @@ def tree(path:str, avoid_sys_dirs:bool, level=1) -> None:
     # main part
     path = os.path.normpath(path)
     if avoid_sys_dirs == True:
-        for item in [x for x in os.listdir(path) if not x.startswith('.') and not x.startswith('__') and not x.startswith('$')]:
-            if os.path.isdir(path+'\\'+item):
+        for item in [x for x in os.listdir(path) if not x.startswith('.') \
+                                                    and not x.startswith('__')\
+                                                    and not x.startswith('$')]:
+            if os.path.isdir(os.path.join(path,item)):
                 if level == 1:
                     print('-'*(level-1), item, ':', sep='')
                 if level > 1:
                     print('  '*(level-1), '|-', item, sep='')
-                tree(path+'\\'+item, True, level+1)
-            elif os.path.isfile(path+'\\'+item):
+                tree(os.path.join(path,item), True, level+1)
+            elif os.path.isfile(os.path.join(path,item)):
                 print('  '*level, '|--', item, sep='')
             else:
                 print('  '*level, '|--'*level, '<underfind file>', sep='')
     else:
         for item in os.listdir(path):
-            if os.path.isdir(path+'\\'+item):
+            if os.path.isdir(os.path.join(path,item)):
                 if level == 1:
                     print('-'*(level-1), item, ':', sep='')
                 if level > 1:
                     print('  '*(level-1), '|-', item, ':', sep='')
                 tree(path+'\\'+item, True, level+1)
-            elif os.path.isfile(path+'\\'+item):
+            elif os.path.isfile(os.path.join(path, item)):
                 print('  '*level, '|--', item, sep='')
             else:
                 print('  '*level, '|--'*level, '<underfind file>', sep='')
 
 
 if __name__ == "__main__":
-    tree('C:\\python\\test', True, 1)
+    mkranddir()
