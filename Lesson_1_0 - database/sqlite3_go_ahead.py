6a85f7bda510fbdd1950e3f151871d7fdbe2016e
dmitriy <dima.lunev14@gmail.com>
2020-07-24 19:43:01 +0300

    добавил функцию UPDATE базы данных

--- Lesson_1_0 - database/sqlite3_go_ahead.py
+++ Lesson_1_0 - database/sqlite3_go_ahead.py
@@ -33,8 +33,8 @@ def connect_to_database(database_name:str, path:str = '') -> sqlite3.Connection:
         connection.close()
 
 
-def create_databace_table(connection:sqlite3.Connection,
-                          table_name:str, args:dict) -> bool:
+def create_database_table(connection:sqlite3.Connection,
+                          table_name:str, args:dict) -> None:
     """ Функция создания таблицы в базе данных
 
     аргументы:
@@ -53,7 +53,29 @@ def create_databace_table(connection:sqlite3.Connection,
         cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_name.lower()} (
                         {', '.join(f'{k} {v}' for k,v in args.items())})""")
         connection.commit()
-    except sqlite3.OperationalError as e:
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        connection.close()
+
+
+def delete_database_table(connection:sqlite3.Connection,
+                          table_name:str) -> None:
+    """ Функция удаляет таблицу из базе данных
+
+    аргументы:
+    connection -- соединение с базой данных
+    table_name -- название таблицы
+    """
+    try:
+        if connection is None:
+            raise WrongDataBaseNameException('Неправильное название базы данных. Введите существующую')
+        if type(table_name) is not str:
+            raise TypeError('Название таблицы может быть только строковое')
+        cursor = connection.cursor()
+        cursor.execute(f"""DROP TABLE IF EXISTS {table_name.lower()}""")
+        connection.commit()
+    except sqlite3.Error as e:
         print(e)
     finally:
         connection.close()
@@ -69,15 +91,19 @@ def add_info_to_table(connection:sqlite3.Connection,
     table_name -- название таблицы
     args -- кортеж элементов таблицы без их типов. Пример: ('something', ...)
     """
-    if connection is None:
-        raise WrongDataBaseNameException('Неправильное название базы данных. Введите существующую')
-    if type(args) is not tuple:
-        raise TypeError('аргумент args может быть только кортежем или списком')
-    cursor = connection.cursor()
-    cursor.execute(f"""INSERT INTO {table_name.lower()} VALUES
-                       (?{', ?' * (len(args)-1)})""", args)
-    connection.commit()
-    connection.close()
+    try:
+        if connection is None:
+            raise WrongDataBaseNameException('Неправильное название базы данных. Введите существующую')
+        if type(args) is not tuple:
+            raise TypeError('аргумент args может быть только кортежем или списком')
+        cursor = connection.cursor()
+        cursor.execute(f"""INSERT INTO {table_name.lower()} VALUES
+                        (?{', ?' * (len(args)-1)})""", args)
+        connection.commit()
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        connection.close()
 
 
 def show_all_created_tables_name(connection:sqlite3.Connection) -> None:
@@ -87,6 +113,8 @@ def show_all_created_tables_name(connection:sqlite3.Connection) -> None:
     connection -- соединение с базой данных
     """
     try:
+        if connection is None:
+            raise WrongDataBaseNameException('Неправильное название базы данных. Введите существующую')
         cursor = connection.cursor()
         cursor.execute("""SELECT name FROM sqlite_master
                         WHERE type='table'""")
@@ -94,7 +122,7 @@ def show_all_created_tables_name(connection:sqlite3.Connection) -> None:
         for e, table in enumerate(cursor.fetchall()):
             print(e+1, ' -- ', table[0])
         connection.commit()
-    except sqlite3.OperationalError as e:
+    except sqlite3.Error as e:
         print(e)
     finally:
         connection.close()
@@ -107,6 +135,8 @@ def show_all_created_tables_content(connection:sqlite3.Connection) -> None:
     connection -- соединение с базой данных
     """
     try:
+        if connection is None:
+            raise WrongDataBaseNameException('Неправильное название базы данных. Введите существующую')
         cursor = connection.cursor()
         cursor.execute("""SELECT name FROM sqlite_master
                         WHERE type='table'""")
@@ -119,11 +149,42 @@ def show_all_created_tables_content(connection:sqlite3.Connection) -> None:
                 print('  ', e+1, ' -- ', sep='', end='')
                 print(*content, sep=', ')
         connection.commit()
-    except sqlite3.OperationalError as e:
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        connection.close()
+
+
+def edit_info_in_table(connection:sqlite3.Connection,
+                       table_name:str, _id:int, data:dict) -> None:
+    """ Функция изменяет нужные данные в таблице
+
+    аргументы:
+    connection -- соединение с базой данных
+    table_name -- название таблицы
+    id -- идентификационный номер записи в таблице
+    data -- данные для изменения
+    """
+    try:
+        if connection is None:
+            raise WrongDataBaseNameException('Неправильное название базы данных. Введите существующую')
+        if type(table_name) is not str:
+            raise TypeError('Название таблицы может быть только строковое')
+        if type(_id) is not int:
+            raise TypeError('Идентификатор записи в таблице должен быть числом')
+        if type(data) is not dict:
+            raise TypeError('данные должны быть в формате словаря')
+        cursor = connection.cursor()
+        cursor.execute(f"""UPDATE {table_name.lower()}
+                           SET {', '.join(f'{e} = "{b}"' for e,b in data.items())}
+                           WHERE id = ?""", (_id,))
+        connection.commit()
+    except sqlite3.Error as e:
         print(e)
     finally:
         connection.close()
 
 
 if __name__ == '__main__':
-    show_all_created_tables_content(connect_to_database('MyDataBase.db'))
+    data = {'title': 'Godfall', 'author': 'Ray Midler', 'cost': 1000.25}
+    edit_info_in_table(connect_to_database('MyDataBase.db'), 'book', 1, data)
