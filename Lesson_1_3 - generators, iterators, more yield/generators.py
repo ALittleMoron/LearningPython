95278977516447a8af81f79fcb86803f8199d983
ALittleMoron <dima.lunev14@gmail.com>
2020-08-17 22:18:30 +0300

    добавил класс-генератор

--- Lesson_1_3 - generators, iterators, more yield/generators.py
+++ Lesson_1_3 - generators, iterators, more yield/generators.py
@@ -16,15 +16,34 @@ def seq_to_generator(seq:Sequence):
     return (x for x in seq)
 
 
+class SimpleClassGenerator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.count = 0
+
+    def __iter__(self):
+        while self.count < self.limit:
+            yield self.count
+            self.count += 1
+
+
 if __name__ == "__main__":
+    # код не на столько сложный, чтобы писать отдельный pytest для всего этого
     print('simple_counter тест:')
     a = simple_counter(25)
     print(a, type(a), sum(a), sep = '  -----  ')
     for num in simple_counter(5):
         print(num)
+    del(a)
 
-    print('\nfunc_name тест:')
+    print('\nseq_to_generator тест:')
     a = seq_to_generator([25, 75, 'sadboy', ('yakuza zero', 'doom 3', 222)])
     print(a, type(a), sep = '  -----  ')
     for num in a:
         print(num, type(num), sep = '  -----  ')
+    del(a)
+
+    print('\nSimpleClassGenerator тест:')
+    a = SimpleClassGenerator(20)
+    print(a, type(a), sum(a), sep = '  -----  ')
+
