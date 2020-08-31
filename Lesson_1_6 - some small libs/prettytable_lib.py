939c70703541762c6495c5280941824932334cd5
ALittleMoron <dima.lunev14@gmail.com>
2020-08-31 22:52:56 +0300

    добавит add_row

--- Lesson_1_6 - some small libs/prettytable_lib.py
+++ Lesson_1_6 - some small libs/prettytable_lib.py
@@ -2,8 +2,10 @@ from prettytable import PrettyTable
 
 
 if __name__ == "__main__":
+    # Также удобно все это интегрируется с базой данных или html. Клево
     table = PrettyTable()
     table.add_column('food name', ['cake','meat', 'spagetti'])
     table.add_column('calories', [2700, 1200, 900])
     table.add_column('price', [2000.25, 570.0, 55.50])
+    table.add_row(['egg', 20, 12.50])
     print(table)
