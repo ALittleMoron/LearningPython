991902ad22245dd64515b42dd67f0d016b92f3df
ALittleMoron <dima.lunev14@gmail.com>
2020-09-02 12:42:34 +0300

    сокращатель ссылок

--- Lesson_1_6 - some small libs/pyshorteners_lib.py
+++ Lesson_1_6 - some small libs/pyshorteners_lib.py
@@ -3,7 +3,7 @@ from pyshorteners import Shortener
 
 if __name__ == "__main__":
     # Это по большей части всё, что нужно от этой библиотеки.
-    # Она просто сокращает ссылки. Не очень полезно, но да ладно.
+    # Она просто сокращает ссылки. Не очень полезно, но да ладно
     url = Shortener()
     short = url.tinyurl.short('www.google.com')
     print(short)
